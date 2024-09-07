a23680889007c59d57dced69f4cca8411ad89c1b
Rashad <rashad.rar37@gmail.com>
2024-09-07 20:36:27 +0530

    frontend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,7 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('dist'));
 
 let persons = [
     {
@@ -32,9 +33,6 @@ let persons = [
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
@@ -81,7 +79,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end();
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
